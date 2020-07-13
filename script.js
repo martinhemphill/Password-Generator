608f2d65a0e740b0873a35d3ea19aa0eea5c38e5
martinhemphill <mthemphi@gmail.com>
2020-07-13 09:54:51 -0400

    Updated readme with repository and deplayed URL

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = newpassword;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add generate password function
+
+function generatePassword() {
+  var newpassword = 1234567;
+  
+
+
+  
+}
