e415a2d8d32bd96fbfafb5c74bd9782536276a0b
martinhemphill <mthemphi@gmail.com>
2020-07-13 10:15:56 -0400

    Added prompts for user password conditions

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 
+console.log (passwordLength);
+console.log (passwordLowercase);
+console.log (passwordUppercase);
+console.log (passwordSpecial);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
+  // User Password Prompts for Condition
+  var passwordLength = prompt("Length of password between 8 - 128 characters");
+  var passwordLowercase = confirm("Do you want lower case characters?");
+  var passwordUppercase = confirm("Do you want upper case characters?");
+  var passwordSpecial = confirm("Do you want special characters?");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = newpassword;
+  passwordText.value = password;
 
 }
 
@@ -19,6 +28,7 @@ function writePassword() {
 
 function generatePassword() {
   var newpassword = 1234567;
+  alert(newpassword);
   
 
 
