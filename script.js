e9db54d2a8442c40fc5edfa20336a0d4f7678f3f
martinhemphill <mthemphi@gmail.com>
2020-07-13 10:37:16 -0400

    Declared potential password character ranges for conditions

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercaseRange = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercaseRange = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbersRange = ["1234567890"];
+var specialRange = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+
+var passwordResult = "";
+var charRange = "";
+
+// User Password Prompts for Condition
+var passwordLengthInput = prompt("Length of password between 8 - 128 characters");
+var passwordLength = parseInt(passwordLengthInput);
+var passwordLowercase = confirm("Do you want lower case characters?");
+var passwordUppercase = confirm("Do you want upper case characters?");
+var passwordNumbers = confirm("Do you want numbers?");
+var passwordSpecial = confirm("Do you want special characters?");
 
 console.log (passwordLength);
 console.log (passwordLowercase);
 console.log (passwordUppercase);
+console.log (passwordNumbers);
 console.log (passwordSpecial);
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  // User Password Prompts for Condition
-  var passwordLength = prompt("Length of password between 8 - 128 characters");
-  var passwordLowercase = confirm("Do you want lower case characters?");
-  var passwordUppercase = confirm("Do you want upper case characters?");
-  var passwordSpecial = confirm("Do you want special characters?");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -27,10 +41,6 @@ function writePassword() {
 // Add generate password function
 
 function generatePassword() {
-  var newpassword = 1234567;
-  alert(newpassword);
-  
-
 
   
 }
